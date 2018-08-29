bd3c24060343dfe5919491c38bec5b3d28aab41c
Samantha brake <sammy.brake@gmail.com>
2018-08-29 18:30:35 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,6 @@
+
 def reverse_each_word(sentence)
-  final_sentence = []
-  return_sentence=""
- new_sentence = sentence.split(" ")
-  new_sentence.collect do |word|
-    final_sentence << word.reverse
-   return_sentence = final_sentence.join(" ")
-   end 
-   return return_sentence
+  sentence.split.collect do |word|
+    word.reverse.join(" ")
+end
 end 
